7d318c4603255dd2e27bc1823c6f53a566682a07
Yoruroong <tuntun4132@naver.com>
2020-04-13 14:59:43 +0900

    ummm

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
-var fs = require('fs'),
-       request = require('request');
+const readline=require("readline");
+ 
+const rl=readline.createInterface({
+  input:process.stdin,
+  output:process.stdout
+});
+ 
+rl.setPrompt("다운할 이미지 URL(CTRL+C : STOP)");
+ 
+rl.prompt();
+rl.on("line",(data)=>{
+    var fs = require('fs'),
+    request = require('request');
 
-var download = function(uri, filename, callback){
-     request.head(uri, function(err, res, body){
-       console.log('content-type:', res.headers['content-type']);
-       console.log('content-length:', res.headers['content-length']);
+    var download = function(uri, filename, callback){
+        request.head(uri, function(err, res, body){
+            console.log('content-type:', res.headers['content-type']);
+            console.log('content-length:', res.headers['content-length']);
 
-       request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
-     });
-};
+            request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
+        });
+    };
 
-download('https://cdn.pixabay.com/photo/2020/04/04/03/07/lake-5000642_960_720.jpg', 'image.png', function(){
-     console.log('done');
+    download(data, 'image.png', function(){
+        console.log('done');
+    });
 });
