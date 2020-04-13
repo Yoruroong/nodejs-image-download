40be9ea720d2440ea65ed864b2a956fa4a2e7060
Yoruroong <tuntun4132@naver.com>
2020-04-13 14:51:26 +0900

    download

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-"" 
+var fs = require('fs'),
+       request = require('request');
+
+var download = function(uri, filename, callback){
+     request.head(uri, function(err, res, body){
+       console.log('content-type:', res.headers['content-type']);
+       console.log('content-length:', res.headers['content-length']);
+
+       request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
+     });
+};
+
+download('https://cdn.pixabay.com/photo/2020/04/04/03/07/lake-5000642_960_720.jpg', 'image.png', function(){
+     console.log('done');
+});
