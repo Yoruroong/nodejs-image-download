6f6ba26871207ff396900ab88b0bb94312f9565a
제로ㅣBrazil <zero734kr@gmail.com>
2020-04-14 21:35:36 -0300

    Use arrow function instead

--- index.js
+++ index.js
@@ -1,27 +1,31 @@
 const readline=require("readline");
- 
+const { createWriteStream } = require("fs");
+const request = require("request");
+
 const rl=readline.createInterface({
   input:process.stdin,
   output:process.stdout
 });
  
-rl.setPrompt("다운할 이미지 URL(CTRL+C => STOP)");
+rl.setPrompt("다운할 이미지 URL (Press CTRL+C to STOP)");
  
 rl.prompt();
-rl.on("line",(data)=>{
-    var fs = require('fs'),
-    request = require('request');
 
-    var download = function(uri, filename, callback){
-        request.head(uri, function(err, res, body){
-            console.log('content-type:', res.headers['content-type']);
-            console.log('content-length:', res.headers['content-length']);
+rl.on("line", data => {
+    let download = (uri, filename, callback) => {
+        return request.head(uri, (err, res, body) => {
+            if(err) {
+                console.error(err.stack || err)
+                process.exit(1)
+            }
+            console.log('Content-Type:', res.headers['content-type']);
+            console.log('Content-Length:', res.headers['content-length']);
 
-            request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
+            return request(uri).pipe(createWriteStream(filename)).on('close', callback);
         });
     };
 
-    download(data, 'image.png', function(){
+    download(data, 'image.png', () => {
         console.log('Done');
         process.exit()
     });
